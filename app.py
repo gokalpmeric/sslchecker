8ce5772cf4712924075900c14261468e354de276
Gökalp Meriç <gokalp@192.168.68.117>
2023-08-07 03:26:20 +0300

    rollback

--- app.py
+++ app.py
@@ -82,18 +82,6 @@ def index():
                     json.dump(domains, file)
 
     return render_template("index.html", domains=domains)
-@app.route("/delete", methods=["POST"])
-def delete():
-    global domains
-    domain_name = request.form.get("domain_name")
-    domains = [d for d in domains if d['name'] != domain_name]
-
-    # Save updates to file
-    with open(DATA_FILE, 'w') as file:
-        json.dump(domains, file)
-
-    return redirect(url_for('index'))
-
 if __name__ == "__main__":
     threading.Thread(target=schedule_check_domains, daemon=True).start()
     app.run(host='0.0.0.0', port=5000, debug=True)
